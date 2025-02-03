698758b1a3e4cb85328da71733e6b310b97e4bba
Anum-Hassan <anumhassan867@gmail.com>
2025-02-03 17:49:51 +0500

    Initial commit

---
+++ application/views/inc/bottom.php
@@ -0,0 +1,15 @@
+<!-- plugins:js -->
+<script src="<?php echo base_url('assets/vendors/js/vendor.bundle.base.js'); ?>"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page-->
+  <script src="<?php echo base_url('assets/vendors/chartjs/Chart.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/vendors/jvectormap/jquery-jvectormap.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js'); ?>"></script>
+  <!-- End plugin js for this page-->
+  <!-- inject:js -->
+  <script src="<?php echo base_url('assets/js/material.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/js/misc.js'); ?>"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="<?php echo base_url('assets/js/dashboard.js'); ?>"></script>
+  <!-- End custom js for this page-->
---
+++ application/views/inc/top.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Hospital Management</title>
+  <link rel="icon" href="<?php echo base_url('assets/images/favicon.png'); ?>" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/mdi/css/materialdesignicons.min.css'); ?>">
+
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/css/vendor.bundle.base.css'); ?>">
+
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/flag-icon-css/css/flag-icon.min.css'); ?>">
+
+  <link rel="stylesheet" href="<?php echo base_url('assets/vendors/jvectormap/jquery-jvectormap.css'); ?>">
+
+  <!-- End plugin css for this page -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/demo/style.css'); ?>">
+  <!-- End layout styles -->
+
+</head>
